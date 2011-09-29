8eabf25e11f820bf320a3558fc2ae2673d1816bc
luca <luca@eeepc900.(none)>
2011-09-29 16:45:41 +0200

    Committer: luca <luca@eeepc900.(none)> 	new file:   imdb-collaboration-graph/python/dd.py

---
+++ imdb-collaboration-graph/python/dd.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+#from pylab import *
+import matplotlib.pyplot as plt
+import sys
+
+f = open("imdbgraphdd.txt", "r")
+matrice = f.readlines()
+gradi=[]
+nodi=[]
+for j in matrice:
+	tokens = j.split()
+	gradi = gradi +[tokens[0]]
+	nodi = nodi + [tokens[1]]
+	
+plt.plot(gradi, nodi, 'rx')
+plt.xscale('log')
+plt.yscale('log')
+
+
+
+plt.grid(True)
+plt.xlabel('Grado')
+plt.ylabel('# Persone')
+#plt.legend(title='Probability',loc=4)
+#plt.title('Number of received messages with Gossiping(p,k)')
+plt.show()
+
