53081259909dc46adb2ac72645132ffee681e261
lucavord <luca@eeepc900.(none)>
2011-12-06 16:48:04 +0100

    modified:   imdb-collaboration-graph/python/pagerankchart.py

--- imdb-collaboration-graph/python/pagerankchart.py
+++ imdb-collaboration-graph/python/pagerankchart.py
@@ -2,21 +2,26 @@
 import matplotlib.pyplot as plt
 import sys
 
-f = open("../results/pageranks/imdbgraphrankpow.ord", "r")
+f = open("../results/pageranks/imdbgraphrankpow.res", "r")
+w = open("../results/pageranks/imdbgraphrankpow.res.approx", "w")
 matrice = f.readlines()
 ranks=[]
 x=[]
-for j in range (0,len(matrice)):
-	tokens = matrice[j].split()
-	x = x + [j]
-	ranks = ranks + [tokens[0]]
+
+print(round(float(6.032755206358235E-8),8))
+print(round(float(4.830324610696718E-5),8))
+for rank in matrice:
+	approx = round(float(rank),8)
+	w.write(str(approx)+"\n")
 	
-plt.plot(x, ranks, 'rx')
+f.close()
+w.close()
+#plt.plot(x, ranks, 'rx')
 #plt.xscale('log')
 #plt.yscale('log')
 
 
 
-plt.grid(True)
-plt.ylabel('Ranks')
-plt.show()
+#plt.grid(True)
+#plt.ylabel('Ranks')
+#plt.show()
